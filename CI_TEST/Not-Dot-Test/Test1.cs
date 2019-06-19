9a8429887d93cd1f8cb927395bb329b1c556b524
Mads Brønd Kracht <mbk@strongminds.dk>
2019-06-19 12:30:52 +0200

    Correct test added using framework 461

---
+++ CI_TEST/Not-Dot-Test/Test1.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace Not_Dot_Test
+{
+    public class Test1
+    {
+
+
+        [Fact]
+        public void PassTest()
+        {
+            var passbool = true;
+
+            Assert.True(passbool);
+
+
+        }
+
+        [Fact]
+        public void FailTest()
+        {
+            var failbool = false;
+
+            Assert.True(failbool);
+        }
+
+    }
+}
