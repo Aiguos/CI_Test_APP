44ff0f5f2f4c98fcd5cbd3b936895f6d52b0d144
Mads Brønd Kracht <mbk@strongminds.dk>
2019-06-19 10:20:42 +0200

    First commit

---
+++ CI_TEST/CI_TEST/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CI_TEST.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CI_TEST/CI_TEST/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CI_TEST.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
