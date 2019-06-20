13469694289b6de1b7333957dfa75e1933c55af4
Mads Brønd Kracht <mbk@strongminds.dk>
2019-06-20 14:29:38 +0200

    Added missing reference to API controller

--- CI_TEST/CI_TEST/Controllers/EmployeesController.cs
+++ CI_TEST/CI_TEST/Controllers/EmployeesController.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using CI_TEST.Models;
 
 namespace CI_TEST.Controllers
 {
-    public class EmployeesController : Controller
+    public class EmployeesController : ApiController
     {
 
         private TestDBEntities _db;
